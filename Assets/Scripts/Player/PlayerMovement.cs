9b51200a7cf1c4eaa01f32b2f3dcde78285dc8ea
Timo <timo.heijne@kpnplanet.nl>
2017-05-16 08:46:00 +0200

    Player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody _rb;
+    private int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+        _rb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 pos = transform.position;
+
+		if(Input.GetKey(KeyCode.W)) {
+            pos.z += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            pos.z += -speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            pos.x += -speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            pos.x += speed * Time.deltaTime;
+        }
+
+        _rb.MovePosition(pos);
+    }
+}
