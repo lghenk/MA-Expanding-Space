e12f3fb55b5ad5c5657262f6442d0dc6ba029ad6
Timo <timo.heijne@kpnplanet.nl>
2017-05-30 09:27:36 +0200

    Started with pickup System

---
+++ Assets/Scripts/ChestPickup.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestPickup : MonoBehaviour {
+
+    public Sprite closedChest;
+    public Sprite almostOpenChest;
+    public Sprite openChest;
+
+    private string[] possiblePickups = { "health", "ammo" };
+    private SpriteRenderer _sr;
+    private int chestHealth = 2;
+    private float maxCooldown = 120;
+    private float curCooldown = 0;
+
+    private PlayerPickup _player;
+
+	// Use this for initialization
+	void Start () {
+        _sr = GetComponent<SpriteRenderer>();
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPickup>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(curCooldown > 0) {
+            curCooldown -= maxCooldown;
+
+            if(curCooldown <= 0) {
+                chestHealth = 2;
+                _sr.sprite = closedChest;
+            }
+        }
+	}
+
+    public void damage(int amount) {
+        chestHealth -= amount;
+
+        if(chestHealth == 2) {
+            _sr.sprite = closedChest;
+        } else if(chestHealth == 1) {
+            _sr.sprite = almostOpenChest;
+        } else if(chestHealth == 0) {
+            _sr.sprite = closedChest;
+            curCooldown = maxCooldown;
+
+            string pickup = possiblePickups[Random.Range(0, possiblePickups.Length)];
+            if(pickup == "health") {
+                _player.addHealth(1);
+            } else if(pickup == "ammo") {
+                _player.resetAmmo();
+            }
+        }
+    }   
+}
---
+++ Assets/Scripts/Player/PlayerPickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickup : MonoBehaviour {
+
+    private PlayerHealth _ph;
+    private PlayerAmmo _pa;
+
+	// Use this for initialization
+	void Start () {
+        _ph = GetComponent<PlayerHealth>();
+        _pa = GetComponent<PlayerAmmo>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void addHealth(int amount) {
+        _ph.addHealth(amount);
+    }
+
+    public void resetAmmo() {
+        //_pa.resetHeat();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Stone") {
+            print("Triggered Stone");
+        }
+    }
+
+}
