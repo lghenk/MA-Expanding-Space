4d780f024d0f07f8cff95eb0d50b017415417e86
lordvonhenk <timo.heijne@kpnplanet.nl>
2017-07-01 12:29:03 +0200

    Finishing Touches

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -48,7 +48,6 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetKey(KeyCode.D)) {
             pos.x += speed * Time.deltaTime;
             animatorBody.SetInteger("direction", 2);
-
         }
 
         if(Input.anyKey) {
@@ -56,7 +55,6 @@ public class PlayerMovement : MonoBehaviour {
                 animatorBody.SetBool("isWalking", true);
                 walkParticle.Play();
             }
-
         } else {
             animatorBody.SetBool("isWalking", false);
             walkParticle.Stop();
